d841d015b16fc6b0bbd553ced85e80fe090665ac
Mario Garces <mgarces132@gmail.com>
2017-01-02 00:34:45 -0300

    added moment and a select with different profiles

--- src/app/home/logbook/logbook.component.ts
+++ src/app/home/logbook/logbook.component.ts
@@ -4,14 +4,29 @@ import { SocketService } from '../../api/socket/socket.service';
 import { SectorService } from '../../api/sector/sector.providers';
 import { Sector } from '../../api/sector/sector.model';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-logbook',
   templateUrl: './logbook.component.html',
   styleUrls: ['./logbook.component.css']
 })
 export class LogbookComponent implements OnInit {
+  
+  profiles = [{
+    id: 'staff',
+    name: 'Empleados'
+  }, {
+    id: 'contractor',
+    name: 'Contratistas'
+  }, {
+    id: 'visitor',
+    name: 'Visitas'
+  }];
+  
   currentSector: Sector;
-  currentFilter: string;
+  currentFixedTimeFilter: string;
+  currentProfileFilter: string = "all";
   
   constructor(private socketService: SocketService, private sectorService: SectorService) { }
 
@@ -19,14 +34,25 @@ export class LogbookComponent implements OnInit {
     this.sectorService.currentSector.subscribe(currentSector => this.currentSector = currentSector )
   }
 
+  changeProfile(profile: string) {
+    console.log(`profile changed to: ${profile}`);
+  }
+
   toggleFilter(filterName: string) {
-    console.log(`setting currentFilter to: ${filterName}`)
     
-    if(this.currentFilter === filterName) { 
-      this.currentFilter = null; 
-    } else {
-      this.currentFilter = filterName;
+    if(this.currentFixedTimeFilter === filterName) { 
+      this.currentFixedTimeFilter = null;
+      return;
     }
+    
+    this.currentFixedTimeFilter = filterName;
+    
+    var unixTime;
+    if (this.currentFixedTimeFilter === 'today') {
+      unixTime = moment().unix();
+      console.log(`unixtime: ${unixTime}`)
+    }
+    console.log(`setting currentFixedTimeFilter to: ${filterName}`)
     
   }
 
