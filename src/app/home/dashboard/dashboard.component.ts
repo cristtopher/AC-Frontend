f1acbe9e9ef45886dedfd570c9fa1e192093395c
Cristtopher Quintana T <cquintana@patagoniswings.com>
2017-03-23 22:06:43 -0300

    style: changed Planta label by Empleados on pieChart of dashborad & review

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -17,12 +17,12 @@ import * as moment from 'moment';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {  
+export class DashboardComponent implements OnInit {
   currentUser: any;
   registers: Register[];
-  
+
   currentSector: Sector;
-  
+
   statistics = {
     totalRegisters: null,
     staffPercentage: null,
@@ -31,26 +31,26 @@ export class DashboardComponent implements OnInit {
   }
 
   profileDistPieChart = {
-    labels: ['Planta', 'Contratistas', 'Visitas'],
+    labels: ['Empleado', 'Contratistas', 'Visitas'],
     data: [0, 0, 0],
     options: {
       tooltips: {
         callbacks: {
-          label: function(tooltipItem, data) {            
+          label: function(tooltipItem, data) {
             //get the concerned dataset
             let dataset = data.datasets[tooltipItem.datasetIndex];
-          
+
             //get the current items value
             let currentValue = dataset.data[tooltipItem.index];
             let currentLabel = data.labels[tooltipItem.index];
-            
+
             return `${currentLabel}: ${currentValue}%`;
           }
         }
-      } 
+      }
     }
   }
-    
+
   registersPerWeekBarChart = {
     labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
     series: [
@@ -58,7 +58,7 @@ export class DashboardComponent implements OnInit {
       {label: 'Salidas', data: [0, 0, 0, 0, 0, 0, 0]}
     ]
   }
-  
+
   constructor(private socketService: SocketService, private userService: UserService, private sectorService: SectorService) { }
 
   ngOnInit() {
@@ -66,7 +66,7 @@ export class DashboardComponent implements OnInit {
                       .subscribe((event) => {
                         if (event.item.isUnauthorized) { return; }
                         if (event.item.sector !== this.currentSector._id) return;
-                        
+
                         if (event.action == "save")   { this.registers.unshift(event.item); this.registers = this.registers.slice(0, 15); }
                         else if (event.action == "remove") {  _.remove(this.registers, { _id: event.item._id }); }
 
@@ -77,7 +77,7 @@ export class DashboardComponent implements OnInit {
                         });
                       });
 
-    
+
     this.userService.currentCompany
                     .flatMap(() => this.userService.currentSector)
                     .filter(s => s != null)
@@ -88,7 +88,7 @@ export class DashboardComponent implements OnInit {
                       this.recalculateStatistics();
                     });
 
-      
+
     this.userService.currentSector
                       .filter(s => s != null)
                       .do(currentSector => this.currentSector = currentSector)
@@ -97,34 +97,34 @@ export class DashboardComponent implements OnInit {
                         this.registers = registers;
                         this.recalculateStatistics();
                       });
-                      
+
   }
-    
+
   recalculateStatistics() {
     this.sectorService.getStatistics(this.currentSector).subscribe(statistics => {
       console.log(`got statistics: ${JSON.stringify(statistics)}`)
-      
+
       this.statistics.totalRegisters        = statistics.staffCount + statistics.contractorCount + statistics.visitCount;
       this.statistics.staffPercentage       = this.statistics.totalRegisters ? (statistics.staffCount / this.statistics.totalRegisters) * 100 : 0;
       this.statistics.contractorsPercentage = this.statistics.totalRegisters ? (statistics.contractorCount / this.statistics.totalRegisters) * 100 : 0;
       this.statistics.visitorsPercentage    = this.statistics.totalRegisters ? (statistics.visitCount / this.statistics.totalRegisters) * 100 : 0;
-    
+
       this.profileDistPieChart.data = [
-        this.statistics.staffPercentage, 
-        this.statistics.contractorsPercentage, 
+        this.statistics.staffPercentage,
+        this.statistics.contractorsPercentage,
         this.statistics.visitorsPercentage
       ];
-      
+
       let reversedEntryWeeklyHistory = statistics.weeklyHistory.entry.reverse();
       let reversedDepartWeeklyHistory = statistics.weeklyHistory.depart.reverse();
-      
+
       this.registersPerWeekBarChart.labels = reversedEntryWeeklyHistory.map(t => moment.weekdays()[moment(t.datetime).day()]);
-            
+
       this.registersPerWeekBarChart.series = [
         { label: 'Entradas', data: reversedEntryWeeklyHistory.map(x => x.count) },
         { label: 'Salidas', data: reversedDepartWeeklyHistory.map(x => x.count) }
       ];
-      
+
     });
   }
 }
--- src/app/home/overview/overview.component.ts
+++ src/app/home/overview/overview.component.ts
@@ -19,12 +19,12 @@ import * as moment from 'moment';
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent implements OnInit {  
+export class OverviewComponent implements OnInit {
   currentUser: any;
   registers: Register[];
-  
+
   currentCompany: Company;
-  
+
   statistics = {
     totalRegisters: null,
     staffPercentage: null,
@@ -33,26 +33,26 @@ export class OverviewComponent implements OnInit {
   }
 
   profileDistPieChart = {
-    labels: ['Planta', 'Contratistas', 'Visitas'],
+    labels: ['Empleado', 'Contratistas', 'Visitas'],
     data: [0, 0, 0],
     options: {
       tooltips: {
         callbacks: {
-          label: function(tooltipItem, data) {            
+          label: function(tooltipItem, data) {
             //get the concerned dataset
             let dataset = data.datasets[tooltipItem.datasetIndex];
-          
+
             //get the current items value
             let currentValue = dataset.data[tooltipItem.index];
             let currentLabel = data.labels[tooltipItem.index];
-            
+
             return `${currentLabel}: ${currentValue}%`;
           }
         }
-      } 
+      }
     }
   }
-    
+
   registersPerWeekBarChart = {
     labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
     series: [
@@ -60,20 +60,20 @@ export class OverviewComponent implements OnInit {
       {label: 'Salidas', data: [0, 0, 0, 0, 0, 0, 0]}
     ]
   }
-  
+
   constructor(private socketService: SocketService, private userService: UserService, private sectorService: SectorService, private companyService: CompanyService) { }
 
   ngOnInit() {
     this.socketService.get('register')
                       .subscribe((event) => {
                         if (event.item.isUnauthorized) { return; }
-                        
+
                         if (event.action == "save")   { }
                         else if (event.action == "remove") { }
-                        
+
                         this.recalculateStatistics();
                       });
-    
+
     this.userService.currentCompany
                     .do(currentCompany => this.currentCompany = currentCompany)
                     .flatMap(currentCompany => this.companyService.getRegisters(this.currentCompany, { top: 15 }))
@@ -81,34 +81,34 @@ export class OverviewComponent implements OnInit {
                       this.registers = registers;
                       this.recalculateStatistics();
                     });
-                                            
+
   }
-    
+
   recalculateStatistics() {
     this.companyService.getStatistics(this.currentCompany).subscribe(statistics => {
       console.log(`got statistics: ${JSON.stringify(statistics)}`)
-      
+
       this.statistics.totalRegisters        = statistics.staffCount + statistics.contractorCount + statistics.visitCount;
       this.statistics.staffPercentage       = this.statistics.totalRegisters ? (statistics.staffCount / this.statistics.totalRegisters) * 100 : 0;
       this.statistics.contractorsPercentage = this.statistics.totalRegisters ? (statistics.contractorCount / this.statistics.totalRegisters) * 100 : 0;
       this.statistics.visitorsPercentage    = this.statistics.totalRegisters ? (statistics.visitCount / this.statistics.totalRegisters) * 100 : 0;
-    
+
       this.profileDistPieChart.data = [
-        this.statistics.staffPercentage, 
-        this.statistics.contractorsPercentage, 
+        this.statistics.staffPercentage,
+        this.statistics.contractorsPercentage,
         this.statistics.visitorsPercentage
       ];
-      
+
       let reversedEntryWeeklyHistory = statistics.weeklyHistory.entry.reverse();
       let reversedDepartWeeklyHistory = statistics.weeklyHistory.depart.reverse();
-      
+
       this.registersPerWeekBarChart.labels = reversedEntryWeeklyHistory.map(t => moment.weekdays()[moment(t.datetime).day()]);
-            
+
       this.registersPerWeekBarChart.series = [
         { label: 'Entradas', data: reversedEntryWeeklyHistory.map(x => x.count) },
         { label: 'Salidas', data: reversedDepartWeeklyHistory.map(x => x.count) }
       ];
-      
+
     });
   }
 }
