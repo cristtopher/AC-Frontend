cristtopher/AC-Frontend

commit 5ffa02c853c1d0248af94c95b645fac413d5f373

Fixed issue with admin user to prevent getting regular user information on common sections

src/app/api/user/user.providers.ts
@@ -91,13 +91,18 @@ export class CurrentUserResolve implements Resolve<User> {
   constructor(private router: Router, private authService: AuthService, private userService: UserService) {}
 
   // before rendering anything, some Subjects are defined: currentUser, currentCompany, currentSector
-  resolve(route: ActivatedRouteSnapshot) {
+  resolve(route: ActivatedRouteSnapshot) {    
+    
+    let userdata$ = Observable.empty()
+                      .flatMap(() => this.userService.getMyCompanies())
+                      .do(currentUserCompanies => this.userService.setCurrentCompany(currentUserCompanies[0]))
+                      .flatMap(currentUserCompanies => this.userService.getMySectors(currentUserCompanies[0]))
+                      .do(firstCompanySectors => this.userService.setCurrentSector(firstCompanySectors[0]));
+ 
+    
     return this.authService.getProfile()
                            .do(currentUser => this.userService.setCurrentUser(currentUser))
-                           .flatMap(() => this.userService.getMyCompanies())
-                           .do(currentUserCompanies => this.userService.setCurrentCompany(currentUserCompanies[0]))
-                           .flatMap(currentUserCompanies => this.userService.getMySectors(currentUserCompanies[0]))
-                           .do(firstCompanySectors => this.userService.setCurrentSector(firstCompanySectors[0]))
+                           .map(currentUser => currentUser.role === 'admin' ? Observable.empty() : userdata$)
                            .toPromise()
                            .then(() => {
                               let currentUser = this.userService.currentUser.getValue();

src/app/shared/left-sidebar/left-sidebar.component.html
@@ -2,9 +2,13 @@
   <section class="sidebar-user-panel">
     <div id="user-panel-slide-toggleable">
 
-      <div class="user-panel-company-logo"><img [src]="'data:image/jpg;base64,'+currentCompany?.logo"></div>
+      <div *ngIf="currentUser.role != 'admin'" class="user-panel-company-logo">
+        <img [src]="'data:image/jpg;base64,'+currentCompany?.logo">
+      </div>
 
-      <div class="user-panel-profile-picture"><img src="/assets/images/user-blue.png"></div>
+      <div class="user-panel-profile-picture">
+        <img src="/assets/images/user-blue.png">
+      </div>
 
       <span class="user-panel-logged-in-text">
         <span class="fa fa-circle-o text-success user-panel-status-icon"></span>

src/app/shared/left-sidebar/left-sidebar.component.ts
@@ -31,25 +31,28 @@ export class LeftSidebarComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit() { 
-    
-    this.userService.getMyCompanies()
+        
+    this.userService.currentUser
+      .do(currentUser => this.currentUser = currentUser)
+      // check if user is admin to stop subsequent operations
+      .filter(currentUser => currentUser.role !== 'admin')
+      // if not admin, then get their companies (sorted by name)
+      .flatMap(currentUser => this.userService.getMyCompanies())
       .map(companies => _.sortBy(companies, 'name'))
-      .subscribe((companies: Company[]) => {
+      .do(companies => {
         if (companies.length > 1) {
           this.isMultiCompany = true;
         }
 
         this.companies = companies;
-
-        this.userService.getMySectors(companies[0])
-          .map(sectors => _.sortBy(sectors, 'name'))
-          .subscribe((sectors: Sector[]) => {
-            this.sectors = sectors;
-          });
-      });
-        
-    this.userService.currentUser.subscribe(currentUser => this.currentUser = currentUser);
+      })
+      // after getting all companies, then get all sectors associated to first company
+      .flatMap(companies => this.userService.getMySectors(companies[0]))
+      .map(sectors => _.sortBy(sectors, 'name'))
+      .do(sectors => this.sectors = sectors)
+      .subscribe();
     
+    // change company when user change it in combobox  
     this.userService.currentCompany
       .filter(c => c != null)
       .subscribe(currentCompany => {
@@ -57,12 +60,14 @@ export class LeftSidebarComponent implements OnInit {
           this.currentCompanyId = currentCompany._id;
       });
     
+    // change sector when user change it in combobox  
     this.userService.currentSector
       .filter(s => s != null)
       .subscribe(currentSector => {          
           this.currentSector   = currentSector;
           this.currentSectorId = currentSector._id;
       });
+      
   }
   
   changeCompany(companyId: string) {

commit 2e9f2cab34459b9cb422c6610404e7c12466d3eb

Fixed bug introduced in 5ffa02c (logged user without its data loaded)

src/app/api/user/user.providers.ts
@@ -93,7 +93,7 @@ export class CurrentUserResolve implements Resolve<User> {
   // before rendering anything, some Subjects are defined: currentUser, currentCompany, currentSector
   resolve(route: ActivatedRouteSnapshot) {    
     
-    let userdata$ = Observable.empty()
+    let userdata$ = Observable.of(null)
                       .flatMap(() => this.userService.getMyCompanies())
                       .do(currentUserCompanies => this.userService.setCurrentCompany(currentUserCompanies[0]))
                       .flatMap(currentUserCompanies => this.userService.getMySectors(currentUserCompanies[0]))
@@ -102,7 +102,8 @@ export class CurrentUserResolve implements Resolve<User> {
     
     return this.authService.getProfile()
                            .do(currentUser => this.userService.setCurrentUser(currentUser))
-                           .map(currentUser => currentUser.role === 'admin' ? Observable.empty() : userdata$)
+                           .map(currentUser => currentUser.role === 'admin' ? Observable.of(null) : userdata$)
+                           .switch()
                            .toPromise()
                            .then(() => {
                               let currentUser = this.userService.currentUser.getValue();